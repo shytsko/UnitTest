90c83fb641a115d2d5f529e2f03b56d3551b71eb
Vladimir Shytsko <shytsko.v@gmail.com>
2023-10-03 15:47:23 +0300

    Add seminar 2 tasks 3, 4

--- python/seminars/third/seminar3.py
+++ python/seminars/third/seminar3.py
@@ -1,3 +1,6 @@
+from decimal import Decimal, Rounded
+
+
 # 3.1. Создайте набор тестов, полностью покрывающих все ветви кода функции fizzBuzz. Эта
 # функция принимает на вход число и возвращает "Fizz", если число делится на 3, "Buzz",
 # если число делится на 5, и "FizzBuzz", если число делится на 15. Если число не делится ни
@@ -17,3 +20,29 @@ def fizz_buzz(i: int) -> str:
 # и false - если 6 нет ни в начале ни в конце массива
 def first_last_6(data: list) -> bool:
     return data[0] == 6 or data[-1] == 6
+
+
+# Создайте тесты, обеспечивающие полное покрытие кода метода calculatingDiscount, который принимает сумму
+# покупки и размер скидки, затем вычисляет и возвращает сумму с учетом скидки. Метод должен обрабатывать
+# исключения, связанные с некорректным размером скидки или отрицательной суммой покупки.
+
+def calculating_discount(purchase_amount: Decimal, discount_amount: int) -> Decimal:
+    if not isinstance(purchase_amount, Decimal):
+        raise TypeError("Параметр purchase_amount должен быть Decimal")
+
+    if not isinstance(discount_amount, int):
+        raise TypeError("Параметр discount_amount должен быть int")
+
+    if purchase_amount < 0:
+        raise ValueError("Сумма покупки не может быть отрицательной")
+
+    if not (0 <= discount_amount <= 100):
+        raise ValueError("Скидка должна быть в диапазоне от 0 до 100%")
+
+    return (purchase_amount * (1 - Decimal(discount_amount) / 100)).quantize(Decimal("1.00"))
+
+
+# Разработайте метод luckySum и напишите для него тесты. Этот метод принимает на вход три числа и возвращает
+# их сумму. Однако, если одно из чисел равно 13, то оно не учитывается при подсчете суммы.
+def lucky_sum(a, b, c):
+    return sum(filter(lambda x: x != 13, (a, b, c)))
--- python/tests/seminars/thirt/test_seminar3.py
+++ python/tests/seminars/thirt/test_seminar3.py
@@ -1,5 +1,6 @@
 import pytest
-from seminars.third.seminar3 import fizz_buzz, first_last_6
+from seminars.third.seminar3 import fizz_buzz, first_last_6, calculating_discount, lucky_sum
+from decimal import Decimal
 
 
 class TestFizzBuzz:
@@ -16,3 +17,43 @@ class TestFirstLast6:
                               ([5, 9, 45, 2], False)))
     def test_first_last_6(self, items, res):
         assert first_last_6(items) == res
+
+
+class TestCalculatingDiscount:
+    @pytest.mark.parametrize("amount, discount, result",
+                             ((Decimal(2000), 50, Decimal(1000)),
+                              (Decimal(2000), 0, Decimal(2000)),
+                              (Decimal(2000), 100, Decimal(0)),
+                              (Decimal(562.33), 12, Decimal("494.85")),
+                              (Decimal(333.33), 33, Decimal("223.33")),))
+    def test_valid_param(self, amount, discount, result):
+        assert calculating_discount(amount, discount) == result
+
+    def test_invalid_amount_value(self):
+        with pytest.raises(ValueError, match="Сумма покупки не может быть отрицательной"):
+            calculating_discount(Decimal(-2000), 50)
+
+    @pytest.mark.parametrize("amount", (100, "100", 100.1))
+    def test_invalid_amount_type(self, amount):
+        with pytest.raises(TypeError, match="Параметр purchase_amount должен быть Decimal"):
+            calculating_discount(amount, 50)
+
+    @pytest.mark.parametrize("discount", (-1, 101))
+    def test_invalid_discount(self, discount):
+        with pytest.raises(ValueError, match="Скидка должна быть в диапазоне от 0 до 100%"):
+            calculating_discount(Decimal(2000), discount)
+
+    @pytest.mark.parametrize("discount", ("100", 100.1, None))
+    def test_invalid_discount_type(self, discount):
+        with pytest.raises(TypeError, match="Параметр discount_amount должен быть int"):
+            calculating_discount(Decimal(2000), discount)
+
+
+class TestLuckySum:
+    @pytest.mark.parametrize("a, b, c, res",
+                             ((5, 6, 12, 23),
+                              (5, 13, 12, 17),
+                              (13, 6, 12, 18),
+                              (5, 6, 13, 11)))
+    def test_lucky_sum(self, a, b, c, res):
+        assert lucky_sum(a, b, c) == res
